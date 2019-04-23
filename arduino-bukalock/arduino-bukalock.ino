0c4f5bf339901a67e33f4c5a7cdd233b900a8970
RenjiraND <13516014@std.stei.itb.ac.id>
2019-04-23 15:15:58 +0700

    added connect to firebase

--- arduino-bukalock/arduino-bukalock.ino
+++ arduino-bukalock/arduino-bukalock.ino
@@ -1,13 +1,13 @@
 /*
-* Ultrasonic Sensor HC-SR04 and Arduino Tutorial
-*
-* by Dejan Nedelkovski,
-* www.HowToMechatronics.com
-*
-* Modified by Renjira Naufhal Dhiaegana
-* 
-* Sources:
-* 7-Segment https://www.instructables.com/id/Multipel-7-Segmen-di-Arduino-dengan-Chip-7447/
+  Ultrasonic Sensor HC-SR04 and Arduino Tutorial
+
+  by Dejan Nedelkovski,
+  www.HowToMechatronics.com
+
+  Modified by Renjira Naufhal Dhiaegana
+
+  Sources:
+  7-Segment https://www.instructables.com/id/Multipel-7-Segmen-di-Arduino-dengan-Chip-7447/
 */
 
 #include <Servo.h>
@@ -16,7 +16,7 @@
 #include <MFRC522.h>
 
 // Defines Pins
-int val = 0;     // variable for reading the pin status 
+int val = 0;     // variable for reading the pin status
 const int pinC = 2;
 const int pinD = 3;
 const int pinA = 4;
@@ -28,18 +28,21 @@ const int rstPin = 9;
 const int ssPin = 10;
 const int servoPin = 14;
 int pinLed = 6;
+const byte DATA_MAX_SIZE = 32;
+char data[DATA_MAX_SIZE];   // an array to store the received data
 
 // Defines LED 7-Segment Number
-int num_array[10][4] = {  { HIGH,LOW,LOW,LOW },     // 0
-                          { LOW,HIGH,LOW,LOW },     // 1
-                          { HIGH,HIGH,LOW,LOW },    // 2
-                          { LOW,LOW,HIGH,LOW },     // 3
-                          { HIGH,LOW,HIGH,LOW },    // 4
-                          { LOW,HIGH,HIGH,LOW },    // 5
-                          { HIGH,HIGH,HIGH,LOW },   // 6
-                          { LOW,LOW,LOW,HIGH },     // 7
-                          { HIGH,LOW,LOW,HIGH },    // 8
-                          { LOW,LOW,LOW,HIGH }};    // 9
+int num_array[10][4] = {  { HIGH, LOW, LOW, LOW },  // 0
+  { LOW, HIGH, LOW, LOW },  // 1
+  { HIGH, HIGH, LOW, LOW }, // 2
+  { LOW, LOW, HIGH, LOW },  // 3
+  { HIGH, LOW, HIGH, LOW }, // 4
+  { LOW, HIGH, HIGH, LOW }, // 5
+  { HIGH, HIGH, HIGH, LOW }, // 6
+  { LOW, LOW, LOW, HIGH },  // 7
+  { HIGH, LOW, LOW, HIGH }, // 8
+  { LOW, LOW, LOW, HIGH }
+};    // 9
 
 
 // Variables
@@ -53,7 +56,7 @@ int pos = 0;    // variable to store the servo position
 long duration;
 int distance;
 bool lock = true;
-int buttonState = 0;  
+int buttonState = 0;
 
 void servoLock();
 void servoUnlock();
@@ -77,8 +80,6 @@ void setup() {
   Serial.begin(9600); // Starts the serial communication
   SPI.begin();      // Initiate  SPI bus
   mfrc522.PCD_Init();   // Initiate MFRC522
-  Serial.println("Approximate your card to the reader...");
-  Serial.println();
 }
 
 void loop() {
@@ -91,17 +92,19 @@ void loop() {
 
   // Pushbutton
   checkPushButton();
-  
+
   // Ultrasonic
   checkUltrasonicDistance();
 
   //RFID
   checkRFID();
+
+  checkFirebase();
 }
 
 void servoLock()
 {
-  if(lock == false){
+  if (lock == false) {
     printIdle();
     myservo.write(180);
     analogWrite(pinLed, 50);
@@ -112,7 +115,7 @@ void servoLock()
 
 void servoUnlock()
 {
-  if(lock == true){
+  if (lock == true) {
     printSuccess();
     analogWrite(pinLed, 255);
     myservo.write(0);
@@ -122,84 +125,79 @@ void servoUnlock()
   servoLock();
 }
 
-// Fungsi untuk menuliskan angka pada 7-segment LED 
-void numWrite(int number) 
+// Fungsi untuk menuliskan angka pada 7-segment LED
+void numWrite(int number)
 {
-    digitalWrite(pinA, num_array[number-1][0]); 
-    digitalWrite(pinB, num_array[number-1][1]); 
-    digitalWrite(pinC, num_array[number-1][2]); 
-    digitalWrite(pinD, num_array[number-1][3]);
+  digitalWrite(pinA, num_array[number - 1][0]);
+  digitalWrite(pinB, num_array[number - 1][1]);
+  digitalWrite(pinC, num_array[number - 1][2]);
+  digitalWrite(pinD, num_array[number - 1][3]);
 }
 
 void checkPushButton()
 {
-    buttonState = digitalRead(buttonPin);
-    if (buttonState == LOW) {
-      servoUnlock();
-    }
+  buttonState = digitalRead(buttonPin);
+  if (buttonState == LOW) {
+    servoUnlock();
+  }
 }
 
 void checkUltrasonicDistance()
 {
-    // Clears the trigPin
-    digitalWrite(trigPin, LOW);
-    delayMicroseconds(2);
-  
-    // Sets the trigPin on HIGH state for 10 micro seconds
-    digitalWrite(trigPin, HIGH);
-    delayMicroseconds(10);
-    digitalWrite(trigPin, LOW);
-    
-    // Reads the echoPin, returns the sound wave travel time in microseconds
-    duration = pulseIn(echoPin, HIGH);
-    
-    // Calculating the distance
-    distance = duration*0.034/2;
+  // Clears the trigPin
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
 
-    // Check distance
-    if(distance < 8 && lock){
-      servoUnlock();
-    }
+  // Sets the trigPin on HIGH state for 10 micro seconds
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+
+  // Reads the echoPin, returns the sound wave travel time in microseconds
+  duration = pulseIn(echoPin, HIGH);
+
+  // Calculating the distance
+  distance = duration * 0.034 / 2;
+
+  // Check distance
+  if (distance < 8 && lock) {
+    servoUnlock();
+  }
 }
+
 
 void checkRFID()
 {
-    // Look for new cards
-    if ( ! mfrc522.PICC_IsNewCardPresent()) 
-    {
-      return;
-    }
-    // Select one of the cards
-    if ( ! mfrc522.PICC_ReadCardSerial()) 
-    {
-      return;
-    }
-    //Show UID on serial monitor
-    Serial.print("RFID tag scanned:");
-    String content= "";
-    byte letter;
-    for (byte i = 0; i < mfrc522.uid.size; i++) 
-    {
-       Serial.print(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " ");
-       Serial.print(mfrc522.uid.uidByte[i], HEX);
-       content.concat(String(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " "));
-       content.concat(String(mfrc522.uid.uidByte[i], HEX));
-    }
-    Serial.println();
-    Serial.print("Message : ");
-    content.toUpperCase();
-    if (content.substring(1) == "76 8E BD F7") //change here the UID of the card/cards that you want to give access
-    {
-      Serial.println("Authorized access");
-      Serial.println();
-      servoUnlock();
-    }
-   
-    else {
-      Serial.println(" Access denied");
-      printRFIDFailed();
-      delay(3000);
-    }
+  // Look for new cards
+  if ( ! mfrc522.PICC_IsNewCardPresent())
+  {
+    return;
+  }
+  // Select one of the cards
+  if ( ! mfrc522.PICC_ReadCardSerial())
+  {
+    return;
+  }
+  //Show UID on serial monitor
+  String content = "";
+  byte letter;
+  for (byte i = 0; i < mfrc522.uid.size; i++)
+  {
+    content.concat(String(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " "));
+    content.concat(String(mfrc522.uid.uidByte[i], HEX));
+  }
+  content.toUpperCase();
+  Serial.println(content.substring(1));
+//  if (content.substring(1) == "76 8E BD F7") //change here the UID of the card/cards that you want to give access
+//  {
+//    servoUnlock();
+//  }
+
+//  else {
+//    Serial.println(" Access denied");
+//    printRFIDFailed();
+//    delay(3000);
+//  }
 }
 
 void printIdle()
@@ -212,7 +210,7 @@ void printSuccess()
 {
   lcd.clear();
   lcd.print(" Unlock Success");
-  lcd.setCursor(0,1);
+  lcd.setCursor(0, 1);
   lcd.print(" Welcome Home!");
 }
 
@@ -220,7 +218,7 @@ void printFailed()
 {
   lcd.clear();
   lcd.print(" Unlock Failed");
-  lcd.setCursor(0,1);
+  lcd.setCursor(0, 1);
   lcd.print("Please wait 5 mins");
 }
 
@@ -228,6 +226,32 @@ void printRFIDFailed()
 {
   lcd.clear();
   lcd.print(" Unlock Failed");
-  lcd.setCursor(0,1);
+  lcd.setCursor(0, 1);
   lcd.print("Please register!");
+}
+
+void checkFirebase() {
+  static char endMarker = '\n'; // message separator
+  char receivedChar;     // read char from serial port
+  int ndx = 0;          // current index of data buffer
+  memset(data, 32, sizeof(data));
+  while(Serial.available() > 0) {
+    receivedChar = Serial.read();
+    if (receivedChar == endMarker) {
+      data[ndx] = '\0'; // end current message
+      if(data[0] == 'o'){
+        servoUnlock();
+        Serial.print
+      }
+      return;
+    }
+    data[ndx] = receivedChar;
+    ndx++;
+    if (ndx >= DATA_MAX_SIZE) {
+      break;
+    }
+  }
+  Serial.println("error: incomplete message");
+  Serial.println(data);
+  memset(data, 32, sizeof(data));
 }
